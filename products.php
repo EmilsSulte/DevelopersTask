f620c9fedd5d019bd134d0ee8165eaf33f76145d
EmilsSulte <77110284+EmilsSulte@users.noreply.github.com>
2021-01-12 18:57:08 +0200

    Add files via upload

---
+++ products.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.4
+ */
+
+/**
+ * Database `products`
+ */
+
+/* `products`.`acme_disc` */
+$acme_disc = array(
+  array('SKU' => 'JVC200123','Name' => 'Acme DISC','Price' => '1','SIZE (MB)' => '700'),
+  array('SKU' => 'JVC200369','Name' => 'Acme DISC','Price' => '1','SIZE (MB)' => '700'),
+  array('SKU' => 'JVC200442 ','Name' => 'Acme DISC','Price' => '1','SIZE (MB)' => '700'),
+  array('SKU' => 'JVC200522','Name' => 'Acme DISC','Price' => '1','SIZE (MB)' => '700')
+);
+
+/* `products`.`book` */
+$book = array(
+  array('SKU' => 'GGWP0007 ','Name' => 'War and Peace','Price' => '20','Weight (KG)' => '2'),
+  array('SKU' => 'GGWP0008','Name' => 'War and Peace','Price' => '20','Weight (KG)' => '2'),
+  array('SKU' => 'GGWP0009','Name' => 'War and Peace','Price' => '20','Weight (KG)' => '2'),
+  array('SKU' => 'GGWP0010','Name' => 'War and Peace','Price' => '20','Weight (KG)' => '2')
+);
+
+/* `products`.`chair` */
+$chair = array(
+  array('SKU' => 'TR120555 ','Name' => 'Chair','Price' => '40','Height (CM)' => '45','Lenght (CM)' => '24','Width (CM)' => '15'),
+  array('SKU' => 'TR120666','Name' => 'Chair','Price' => '40','Height (CM)' => '45','Lenght (CM)' => '24','Width (CM)' => '15'),
+  array('SKU' => 'TR120777','Name' => 'Chair','Price' => '40','Height (CM)' => '45','Lenght (CM)' => '24','Width (CM)' => '15'),
+  array('SKU' => 'TR120888','Name' => 'Chair','Price' => '40','Height (CM)' => '45','Lenght (CM)' => '24','Width (CM)' => '15')
+);
