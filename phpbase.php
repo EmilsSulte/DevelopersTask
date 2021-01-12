ffdd2ae969d8c52a1e69a27822c39acb044f9ddd
EmilsSulte <77110284+EmilsSulte@users.noreply.github.com>
2021-01-12 17:53:46 +0200

    Update phpbase.php

--- phpbase.php
+++ phpbase.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "visitor", "Databasevi$itor123", "products");
+$mysqli = new mysqli("localhost", "root", "", "products");
 
 if ($mysqli -> connect_errno){
     echo "Failed to connect to MySQL:" . $mysqli -> connect_error;
